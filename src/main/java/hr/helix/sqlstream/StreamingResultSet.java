854e8b7e0122cd95a3462ede2e9697a784add53c
Dinko Srkoč <dinko.srkoc@gmail.com>
2014-01-24 20:27:17 +0100

    Fixed collectMany - it now keeps the stream lazy + refactoring.

--- src/main/java/hr/helix/sqlstream/StreamingResultSet.java
+++ src/main/java/hr/helix/sqlstream/StreamingResultSet.java
@@ -17,9 +17,9 @@ public class StreamingResultSet {
     private static class Fn {
         private Fn next;
 
-        public StatVal call(StatVal sv) { return apply(sv); } // override this
+        public Value call(Value v) { return apply(v); } // override this
 
-        protected final StatVal apply(StatVal sv) { return next != null ? next.call(sv) : sv; }
+        protected final Value apply(Value v) { return next != null ? next.call(v) : v; }
 
         public Fn andThen(Fn that) {
             if (next == null)
@@ -35,8 +35,8 @@ public class StreamingResultSet {
 
         public Collect(Closure<T> f) { this.f = f; }
 
-        @Override public StatVal call(StatVal sv) {
-            return apply(new StatVal(f.call(sv.getValue())));
+        @Override public Value call(Value v) {
+            return apply(new Value(f.call(v.getValue())));
         }
     }
 
@@ -45,17 +45,17 @@ public class StreamingResultSet {
 
         public CollectMany(Closure<T> f) { this.f = f; }
 
-        @Override public StatVal call(StatVal sv) {
+        @Override public Value call(Value v) {
             ArrayList<Object> vals = new ArrayList<Object>();
-            for (Object x : f.call(sv.getValue())) {
-                final StatVal _sv = apply(new StatVal(x));
-                if (_sv.getStat() == Status.OK)
-                    _sv.exportTo(vals);
-                else if (_sv.getStat() == Status.STOP_ITER)
-                    break;
+
+            for (Object o : f.call(v.getValue())) {
+                final Value val = apply(new Value(o));
+                if (val.terminate())
+                    return new TerminateWithValue(new FlatValue(vals));
+                val.exportTo(vals);
             }
 
-            return new FlatStatVal(vals);
+            return new FlatValue(vals);
         }
     }
 
@@ -64,8 +64,8 @@ public class StreamingResultSet {
 
         public FindAll(Closure<Boolean> p) { this.p = p; }
 
-        @Override public StatVal call(StatVal sv) {
-            return p.call(sv.getValue()) ? apply(sv) : sv.putStat(Status.STOP_STEP);
+        @Override public Value call(Value v) {
+            return p.call(v.getValue()) ? apply(v) : IgnoreValue.INSTANCE;
         }
     }
 
@@ -74,9 +74,9 @@ public class StreamingResultSet {
         
         public Each(Closure<Object> f) { this.f = f; }
 
-        @Override public StatVal call(StatVal sv) {
-            f.call(sv.getValue());
-            return apply(sv);
+        @Override public Value call(Value v) {
+            f.call(v.getValue());
+            return apply(v);
         }
     }
 
@@ -85,12 +85,12 @@ public class StreamingResultSet {
 
         public Take(int n) { this.n = n; }
 
-        @Override public StatVal call(StatVal sv) {
+        @Override public Value call(Value v) {
             if (n == 0) {
-                return sv.getStat() == Status.STOP_ITER ? sv : sv.putStat(Status.STOP_ITER);
+                return new TerminateEmpty();
             } else {
                 --n;
-                return apply(sv);
+                return apply(v);
             }
         }
     }
@@ -101,11 +101,11 @@ public class StreamingResultSet {
 
         public TakeWhile(Closure<Boolean> p) { this.p = p; }
 
-        @Override public StatVal call(StatVal sv) {
-            if (done || (done = !p.call(sv.getValue()))) {
-                return sv.getStat() == Status.STOP_ITER ? sv : sv.putStat(Status.STOP_ITER);
+        @Override public Value call(Value v) {
+            if (done || (done = !p.call(v.getValue()))) {
+                return TerminateEmpty.INSTANCE;
             } else {
-                return apply(sv);
+                return apply(v);
             }
         }
     }
@@ -115,12 +115,12 @@ public class StreamingResultSet {
 
         public Drop(int n) { this.n = n; }
 
-        @Override public StatVal call(StatVal sv) {
+        @Override public Value call(Value v) {
             if (n == 0) {
-                return apply(sv.getStat() == Status.STOP_STEP ? sv.putStat(Status.OK) : sv);
+                return apply(v);
             } else {
                 --n;
-                return sv.getStat() == Status.OK ? sv.putStat(Status.STOP_STEP) : sv;
+                return IgnoreValue.INSTANCE;
             }
         }
     }
@@ -131,41 +131,58 @@ public class StreamingResultSet {
 
         public DropWhile(Closure<Boolean> p) { this.p = p; }
 
-        @Override public StatVal call(StatVal sv) {
-            if (done || (done = !p.call(sv.getValue()))) {
-                return apply(sv.getStat() == Status.STOP_STEP ? sv.putStat(Status.OK) : sv);
+        @Override public Value call(Value v) {
+            if (done || (done = !p.call(v.getValue()))) {
+                return apply(v);
             } else {
-                return sv.getStat() == Status.OK ? sv.putStat(Status.STOP_STEP) : sv;
+                return IgnoreValue.INSTANCE;
             }
         }
     }
 
-    // todo proučiti mogućnost generifikacije StatVala
+    // todo proučiti mogućnost generifikacije Value
 
-    private static class StatVal {
-        private Status stat = Status.OK;
+    private static class Value {
         private Object value;
 
         public Object getValue() { return value; }
-        public Status getStat()  { return stat;  }
 
-        protected StatVal() {}
-        public StatVal(Object val) { value = val; }
-
-        public StatVal putStat(Status s)  { stat = s; return this; }
+        protected Value() {}
+        public Value(Object val) { value = val; }
 
         public void exportTo(List<Object> xs) { xs.add(value); }
+
+        public boolean terminate() { return false; }
     }
 
-    private static class FlatStatVal extends StatVal {
+    private static class FlatValue extends Value {
         private final Collection<Object> values;
 
-        public FlatStatVal(List<Object> vals) { values = vals; }
+        public FlatValue(List<Object> vals) { values = vals; }
 
         @Override public void exportTo(List<Object> xs) { xs.addAll(values); }
     }
 
-    private enum Status { STOP_STEP, STOP_ITER, OK }
+    private static class IgnoreValue extends Value {
+        public static final IgnoreValue INSTANCE = new IgnoreValue();
+
+        @Override public void exportTo(List<Object> xs) {}
+    }
+
+    private static class TerminateEmpty extends IgnoreValue {
+        public static final TerminateEmpty INSTANCE = new TerminateEmpty();
+
+        @Override public boolean terminate() { return true; }
+    }
+
+    private static class TerminateWithValue extends Value {
+        private final Value v;
+
+        public TerminateWithValue(Value v) { this.v = v; }
+
+        @Override public boolean terminate() { return true; }
+        @Override public void exportTo(List<Object> xs) { v.exportTo(xs); }
+    }
 
     private StreamingResultSet(ResultSet rs, Fn fn) {
         this.rs = rs;
@@ -200,27 +217,16 @@ public class StreamingResultSet {
         return new StreamingResultSet(rs, compute.andThen(that));
     }
 
-    /*
-     *  a) collect ... a -> b   ; b >> xs        ; sljedeći element
-     *  b) findAll ... a -> bool; bool ? a >> xs ; sljedeći element
-     *??c) find    ... a -> bool; bool ? vrati a ; bool ? sljedeći element
-     *  d) each    ... a -> unit;                ; sljedeći element
-     *  f) take    ...          ; a >> xs        ; n > 0 ? sljedeći element
-     *  
-     *  a -> b, sačuvaj b?, sljedeći element?
-     */
     public StreamingResultSet force() throws SQLException {
         if (values != null) return this; // stream is already realized
 
         values = new ArrayList<Object>();
         GroovyResultSet groovyRS = new GroovyResultSetProxy(rs).getImpl();
         while (groovyRS.next()) {
-            StatVal sv = compute.call(new StatVal(groovyRS));
+            Value v = compute.call(new Value(groovyRS));
 
-            if (sv.getStat() == Status.OK)
-                sv.exportTo(values);
-            else if (sv.getStat() == Status.STOP_ITER)
-                break;
+            v.exportTo(values);
+            if (v.terminate()) break;
         }
 
         return this;
