521e32009a47460a78a983cb85a9ad545965c6e6
Dinko Srkoč <dinko.srkoc@gmail.com>
2014-02-05 11:23:08 +0100

    Added copyright and license info to source files

--- src/test/groovy/hr/helix/sqlstream/WithStreamSpec.groovy
+++ src/test/groovy/hr/helix/sqlstream/WithStreamSpec.groovy
@@ -1,8 +1,29 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package hr.helix.sqlstream
 
 import groovy.sql.Sql
 import spock.lang.Specification
 
+/**
+ * Compares the performance of withSteam() vs Sql#rows() vs Sql#eachRow()
+ *
+ * @author Dinko Srkoč
+ * @since 2014-01-29
+ */
 class WithStreamSpec extends Specification {
 
     Sql sql
